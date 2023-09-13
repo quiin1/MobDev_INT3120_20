39cea5ceba3530e3784709228e1413b443917547
quynhnt <trangquynh17@gmail.com>
2023-09-14 02:20:47 +0700

    fix: week3 layout

--- week3/app/src/main/java/com/example/week3/MainActivity.java
+++ week3/app/src/main/java/com/example/week3/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.linear_layout);
+//        setContentView(R.layout.relative_layout);
+//        setContentView(R.layout.absolute_layout);
+        setContentView(R.layout.table_layout);
+//        setContentView(R.layout.activity_main);
     }
 }
